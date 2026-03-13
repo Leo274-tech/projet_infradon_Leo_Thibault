d22f1e755fbf30b58882140bcfb66e654c41aea6
Leo274-tech <leolabrousse27@gmail.com>
2026-03-13 11:30:48 +0100

    ajout de table pour MPD

--- init-queries.sql
+++ init-queries.sql
@@ -10,3 +10,45 @@ CREATE TABLE IF NOT EXISTS signalements (
     statut VARCHAR(20)
 
 );
+
+CREATE TABLE IF NOT EXISTS interventions (
+    id SERIAL PRIMARY KEY,
+    date_intervention DATE,
+    type_intervention INTEGER REFERENCES types_interventions(id),
+    technicien VARCHAR(30),
+    duree_heure REAL,
+    cout_matériau DECIMAL(10, 2),
+    remarques VARCHAR(120),
+    INTERGER REFERENCES NOT NULL signalement(id)
+
+);
+
+CREATE TABLE IF NOT EXISTS signalement_inventaire (
+    id SERIAL PRIMARY KEY,
+    INTERGER REFERENCES NOT NULL signalement(id),
+    INTERGER REFERENCES NOT NULL inventaire(id)
+
+);
+
+CREATE TABLE IF NOT EXISTS inventaire (
+    id SERIAL PRIMARY KEY,
+    id_inventaire INTEGER NOT NULL,
+    type_inventaire INTEGER REFERENCES types_inventaire(id),
+    materiau VARCHAR(50),
+    lieu VARCHAR(30),
+    date_installation DATE,
+    etat INTEGER REFERENCES etat(id),
+    remarques VARCHAR(120)
+);
+
+
+CREATE TABLE IF NOT EXISTS fournisseurs (
+    id SERIAL PRIMARY KEY,
+    entreprise VARCHAR(30),
+    nom_contact VARCHAR(30) NOT NULL,
+    telephone INTEGER,
+    email VARCHAR(50),
+    type_materiel VARCHAR(50) REFERENCES types_inventaire(id),
+    remarque(120)
+
+);
