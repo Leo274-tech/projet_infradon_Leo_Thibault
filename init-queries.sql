569a5ebaf8fe377076563d766b8aaaf107ecd30a
Thibault <thibault.moret@heig-vd.ch>
2026-03-13 09:45:57 +0100

    Ajout des fichiers CSV

--- init-queries.sql
+++ init-queries.sql
@@ -1,6 +1,6 @@
--- Active: 1764341819867@@127.0.0.1@5432
+-- Active: 1772186463800@@127.0.0.1@5432@infradon
 -- Active: 1772794232287@@127.0.0.1@5432@infradon-- Active: 1772794232287@@127.0.0.1@5432@infradon-- Active: 1772794232287@@127.0.0.1@5432@postgres-- Active: 1772794232287@@127.0.0.1@5432@infradon@public-- Active: 1772794232287@@127.0.0.1@5432@infradon-- Active: 1772794232287@@127.0.0.1@5432@infradon-- Active: 1772794232287@@127.0.0.1@5432@infradon-- Active: 1766063369847@@127.0.0.1@5438-- Active: 1766063369847@@127.0.0.1@5438-- Active: 1766063369847@@127.0.0.1@5438-- Active: 1764341819867@@127.0.0.1@5437
-CREATE TABLE signalements (
+CREATE TABLE IF NOT EXISTS signalements (
     id SERIAL PRIMARY KEY,
     date_signalement DATE,
     signale_par VARCHAR(50),
